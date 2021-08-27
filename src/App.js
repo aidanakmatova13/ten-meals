46ebe8f7fc5295178e63ae125ea061bdaf117280
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-27 22:36:02 +0300

    fix code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import {BrowserRouter as Router,Route} from "react-router-dom";
+import {BrowserRouter, BrowserRouter as Router, Route} from "react-router-dom";
 import Meals from "./Meals";
 import MealDetails from "./MealDetails";
+import Browse from "./Browse";
 
 function App() {
     return (
         <>
+            <BrowserRouter/>
             <Router>
-                <Route exact path={"/"}><Meals /></Route>
-                <Route path={"/mealdetails/i=:id"}><MealDetails /></Route>
+                <Route exact path="/"><Meals /></Route>
+                <Route path="/mealdetails/i=:id"><MealDetails /></Route>
+                <Route path='/browse/:name'><Browse/></Route>
             </Router>
         </>
     )
--- src/MealDetails.js
+++ src/MealDetails.js
@@ -1,52 +1,55 @@
-import {useParams} from "react-router-dom"
-import {useEffect,useState} from "react";
+import {useEffect, useState} from "react";
+import {useParams} from 'react-router-dom'
 import axios from "axios";
 
-const MealDetails = () => {
-    const [meal, setMeal] = useState({})
-    const [ingredients, setIngredients] = useState([])
-    const params = useParams()
 
+const MealDetails = () => {
+    const [mealDetails,setMealDetails] = useState({})
+    const [ingredient,setIngredient] = useState([])
+    const mealDetailsparsms = useParams()
+    console.log(mealDetailsparsms)
     useEffect(() => {
-        axios(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
+        axios(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealDetailsparsms.id}`)
             .then(({data}) => {
                 const obj = data.meals[0]
-                setMeal(obj)
-                const newIngs = Array(20).fill(0).reduce((acc, item, idx) => {
-                    if (obj[`strIngredient${idx + 1}`]) {
-                        return [...acc, obj[`strIngredient${idx + 1}`]]
+                setMealDetails(obj)
+                const strIngredient = Array(20).fill(0).reduce((acc,item, idx) =>{
+                    if(obj[`strIngredient${idx + 1}`]){
+                        return[...acc,obj[`strIngredient${idx + 1}`]]
                     }
-                    return acc
-                }, [])
-                setIngredients(newIngs)
+                    return  acc
+                },[])
+                setIngredient(strIngredient)
             })
-    }, [])
+    },[mealDetailsparsms])
+
 
     return (
         <div>
+
             {
                 <div>
-                    Meals details:
-                    <div><img src={meal.strMealThumb} alt="" width='300'/></div>
-                    <div>
-                        <h4>Instructions:</h4>
-                        {meal.strInstructions}
-                    </div>
-                    <div>
-                        <h4>Ingredients:</h4>
-                        {ingredients.map(el =>
-                            <>
-                                {el}
-                                <img width='150' src={`https://www.themealdb.com/images/ingredients/${el}.png`} alt=""/>
-                            </>
-                        )}
-                    </div>
+                    <img src={mealDetails.strMealThumb} alt='#' width='200'/>
+                    <h3>{ mealDetails.strMeal}</h3>
+                    <p>Instructions: {mealDetails.strInstructions}</p>
+
                 </div>
+
             }
+            {
+                ingredient.map(item =>
+                    <div>
+                        <img src={`https://www.themealdb.com/images/ingredients/${item}.png`} alt='#' width='50'/>
+                        <p>{item}</p>
+                    </div>
+                )
+            }
+
         </div>
-    );
+    )
 }
-export default MealDetails;
+
+export default MealDetails
 
 
 
