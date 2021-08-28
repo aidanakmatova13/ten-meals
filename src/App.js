e56602c11a83af78c92203153c7c37c1f0318877
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-29 00:31:37 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import {BrowserRouter,Route } from 'react-router-dom'
 import Meals from "./Meals";
 import MealDetails from "./MealDetails";
 import Browse from "./Browse";
+import Header from "./Header";
 function App() {
     return (
+        <>
         <BrowserRouter>
-            <div className='container'>
-                <Route exact path='/'><Meals/></Route>
-                <Route path='/meal/:id'><MealDetails/></Route>
-                <Route path='/browse/:name'><Browse/></Route>
-            </div>
+            <Route exact path='/'><Header/></Route>
+            <Route exact path='/'><Meals/></Route>             <Route path='/meal/:id'><MealDetails/></Route>
+            <Route path='/browse/:name'><Browse/></Route>
         </BrowserRouter>
+        </>
     );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+const Header =() =>{
+    return (
+        <div>
+            <img/>
+            <ul>
+                <Link to ='/'>Home</Link>
+            </ul>
+        </div>
+    )
+}
+export default Header
